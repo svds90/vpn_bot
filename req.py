03bdd2c5536c3ad650170618f55513fa3b47b2cb
Alex <svds22090@gmail.com>
2024-01-19 00:26:33 +0300

    upd

--- req.py
+++ req.py
@@ -1,17 +1,10 @@
-from dotenv import load_dotenv
 from pprint import pprint
 import os
 import requests
 from config import *
 
-load_dotenv()
-
-print(os.getenv("VPN_API_URL"))
-
 api = os.getenv("VPN_API_URL")
 
-print(f"{api}/server")
+responce = requests.get(f"{api}/access-keys", verify=False)
 
-responce = requests.get(f"{api}/server", verify=False)
-
-pprint(responce.text)
+pprint(responce.json())
