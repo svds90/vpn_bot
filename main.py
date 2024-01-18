1b7b3c509615d3f92fbe7f461f5a0e65b96daba4
Alex <svds22090@gmail.com>
2024-01-18 06:20:46 +0300

    upd

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-#!/usr/bin/python3
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+from dotenv import load_dotenv
+import asyncio
+import logging
+import os
+
+load_dotenv()
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=os.getenv("BOT_TOKEN"))  # type: ignore
+dp = Dispatcher()
+
+
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("hi")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
