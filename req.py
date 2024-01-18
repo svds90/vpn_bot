00c4cab32949f6933932d63e744556822ccdfea5
Alex <svds22090@gmail.com>
2024-01-18 22:48:13 +0300

    upd

---
+++ req.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+from pprint import pprint
+import os
+import requests
+
+load_dotenv()
+
+print(os.getenv("VPN_API_URL"))
+
+api = os.getenv("VPN_API_URL")
+
+print(f"{api}/server")
+
+responce = requests.get(f"{api}/server", verify=False)
+
+pprint(responce.text)
