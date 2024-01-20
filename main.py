f751c6950901fc4e3dcd9c21100a239ba1c2d278
Alex <svds22090@gmail.com>
2024-01-21 01:28:22 +0300

    upd

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
-from dotenv import load_dotenv
 import asyncio
 import logging
-import os
-
-load_dotenv()
+from settings.config import *
 
 logging.basicConfig(level=logging.INFO)
-bot = Bot(token=os.getenv("BOT_TOKEN"))  # type: ignore
+
+bot = Bot(token=bot_token)  # type: ignore
 dp = Dispatcher()
 
 
