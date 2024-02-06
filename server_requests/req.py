77dea223a67cb938c5a0aa0a07f3ff3ee32ebb2e
Alex <svds22090@gmail.com>
2024-02-06 18:05:29 +0300

    upd

--- server_requests/req.py
+++ server_requests/req.py
@@ -1,7 +1,20 @@
 import os
 import requests
 
-api = os.getenv("VPN_API_URL")
 
-response = requests.get(f"{api}/access-keys", verify=False)
-response_dict = response.json()
+class OutlineMeta(type):
+    __instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
+
+
+class OutlineMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
