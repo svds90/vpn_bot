6a49ea8a1377680699a5a17180744934d9c4daec
Alex <svds22090@gmail.com>
2024-02-07 22:10:32 +0300

    upd

--- server_requests/req.py
+++ server_requests/req.py
@@ -1,20 +1,46 @@
 import os
 import requests
+from dataclasses import dataclass
+from typing import Any
 
 
-class OutlineMeta(type):
-    __instance = None
+class OutlineBase:
+    """Base class for all Outline instances"""
 
-    def __new__(cls, *args, **kwargs):
-        if cls.__instance is None:
-            cls.__instance = super().__new__(cls)
-        return cls.__instance
-
-
-class OutlineMeta(type):
     _instances = {}
 
-    def __call__(cls, *args, **kwargs):
+    def __new__(cls, *args, **kwargs):
         if cls not in cls._instances:
-            cls._instances[cls] = super().__call__(*args, **kwargs)
+            cls._instances[cls] = super().__new__(cls)
         return cls._instances[cls]
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        return self
+
+
+@dataclass
+class OutlineServerInfo(OutlineBase):
+
+    server_name: str
+    server_id: str
+    server_metric_status: bool
+    server_created_time: int
+    server_version: str
+    server_port_for_new_keys: int
+    server_hostname_for_keys: str
+
+
+class OutlineServer(OutlineBase):
+    """Class for interacting with the Outline server"""
+
+    def __init__(self, outline_api_url: str):
+        self.outline_api_url = outline_api_url
+
+        req = requests.get(f"{outline_api_url}/server", verify=False)
+        self.server_info = OutlineServerInfo(req.json())
+
+    def __call__(self) -> str:
+        return self.outline_api_url
+
+
+client = os.getenv("VPN_API_URL")
