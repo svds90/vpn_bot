74fba678c0c7efa8349e78f71b4a5f1950b63bf0
Alex <svds22090@gmail.com>
2024-02-09 11:04:09 +0300

    upd

--- server_requests/req.py
+++ server_requests/req.py
@@ -18,25 +18,17 @@ class OutlineBase:
         return self
 
 
-@dataclass
 class OutlineServerInfo(OutlineBase):
 
-    server_name: str
-    server_id: str
-    server_metric_status: bool
-    server_created_time: int
-    server_version: str
-    server_port_for_new_keys: int
-    server_hostname_for_keys: str
-
     def __init__(self, server_info: dict):
-        self.server_name = server_info.get('name', 'Outline server')
-        self.server_id = server_info.get('serverId', "")
-        self.server_metric_status = server_info.get('metricsEnabled', False)
-        self.server_created_time = server_info.get('createdTimestampsMs', 0)
-        self.server_version = server_info.get('version', "")
-        self.server_port_for_new_keys = server_info.get('portForNewAccessKeys', 0)
-        self.server_hostname_for_keys = server_info.get('hostnameForAccessKeys', "")
+        self.server_key: str = server_info.get('server_key', "")
+        self.server_name: str = server_info.get('name', 'Outline server')
+        self.server_id: str = server_info.get('serverId', "")
+        self.server_metric_status: bool = server_info.get('metricsEnabled', False)
+        self.server_created_time: int = server_info.get('createdTimestampsMs', 0)
+        self.server_version: str = server_info.get('version', "")
+        self.server_port_for_new_keys: int = server_info.get('portForNewAccessKeys', 0)
+        self.server_hostname_for_keys: str = server_info.get('hostnameForAccessKeys', "")
 
 
 class OutlineServer(OutlineBase):
@@ -44,12 +36,17 @@ class OutlineServer(OutlineBase):
 
     def __init__(self, outline_api_url: str):
         self.outline_api_url = outline_api_url
-
-        req = requests.get(f"{outline_api_url}/server", verify=False)
-        self.server_info = OutlineServerInfo(req.json())
+        self.server_info = OutlineServerInfo(self.__fetch_server_info(outline_api_url))
 
     def __call__(self) -> str:
         return self.outline_api_url
+
+    def __fetch_server_info(self, outline_api_url):
+        request = requests.get(f"{outline_api_url}/server", verify=False)
+        server_json = request.json()
+        server_json.update({"server_key": outline_api_url})
+
+        return server_json
 
 
 client = os.getenv("VPN_API_URL")
