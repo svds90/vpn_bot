7cf6004009040f8e9ee200569830184459d40ebe
Alex <svds22090@gmail.com>
2024-02-10 21:01:32 +0300

    upd

--- server_requests/req.py
+++ server_requests/req.py
@@ -5,6 +5,10 @@ from datetime import datetime
 from typing import Any
 
 
+class OutlineServerError(Exception):
+    pass
+
+
 class OutlineBase:
     """Base class for all Outline instances"""
 
@@ -21,7 +25,7 @@ class OutlineBase:
 
 class OutlineServerInfo(OutlineBase):
 
-    def __init__(self, server_info: dict):
+    def __init__(self, server_info: Any):
         self.server_key: str = server_info.get('server_key', "")
         self.server_name: str = server_info.get('name', 'Outline server')
         self.server_id: str = server_info.get('serverId', "")
@@ -43,19 +47,25 @@ class OutlineServer(OutlineBase):
         return self.outline_api_url
 
     def __fetch_server_info(self, outline_api_url):
-        request = requests.get(f"{outline_api_url}/server", verify=False)
-        print(request.json())
-        server_json = request.json()
-        server_json = {**server_json, 'server_key': outline_api_url,
-                       'createdTimestampMs': self.__timestamp_to_date(server_json['createdTimestampMs'])}
+
+        request = requests.get(f"{outline_api_url}/server", verify=False, timeout=2)
+        server_json = {
+            **request.json(),
+            'server_key': outline_api_url,
+            'createdTimestampMs': self.__timestamp_to_date(request.json()['createdTimestampMs'])
+        }
 
         return server_json
 
     def __timestamp_to_date(self, timestamp: int) -> str:
         date_time = datetime.fromtimestamp(timestamp / 1000)
-        date_time = date_time.strftime("%Y-%m-%d %H:%M:%S")
+        date_time = date_time.strftime("%d/%m/%y %H:%M:%S")
 
         return date_time
 
+    def change_hostname(self, new_hostname: str) -> None:
+        self.server_info.server_hostname_for_keys = new_hostname
 
-client = os.getenv("VPN_API_URL")
+    def rename_server(self, new_name: str) -> None:
+        requests.put(f"{self.server_info.server_key}/name", {"name": new_name}, verify=False)
+        self.server_info.server_name = new_name
